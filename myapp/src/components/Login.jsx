c405ae1d62ea980b4526510d1d6253bc15f51e32
keerthna-unnikrishnan <144515493+keerthna-unnikrishnan@users.noreply.github.com>
2024-10-29 14:25:56 +0530

    Add files via upload

---
+++ myapp/src/components/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+        <h1>hello</h1>
+       <input type="text" name="" id="" placeholder=' enter your username' /> <br /> <br />
+       <input type="password" name="" id="" placeholder='password' />
+        </div>
+   
+  )
+}
+
+export default Login
---
+++ myapp/src/components/Signup.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Signup = () => {
+  return (
+    <div>
+        <h2> <ul>sign up</ul></h2>
+        <input type="text" name="" id="" placeholder=' enter your username' /> <br /> <br />
+       <input type="password" name="" id="" placeholder='password' /><br /> <br />
+       <input type="email" name="" id="" placeholder='email'  /> <br /> <br />
+       <input type="number" name="" id="" placeholder='phone number' /><br /> <br />
+       <input type="button" value="submit" />
+    </div>
+  )
+}
+
+export default Signup
